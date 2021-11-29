5748a6d8bc4a368c8faa1ee33f71d1aa075de814
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-11-28 22:23:59 -0500

    Update Ogre.java

--- v2/Ogre.java
+++ v2/Ogre.java
@@ -10,7 +10,7 @@ _defense = 30;
 
 public static void about()
 {
-  System.out.println("Ogre: High health and defense, low attack.");
+  System.out.println("Ogre: High health and defense, low attack. A brute this massive would spell doom for all but the most seasoned players. Thankfully for the less skilled, he's been lifting more pies to his mouth than weights, so he doesn't hit as hard as he tells us he used to.");
 }
 
 }
