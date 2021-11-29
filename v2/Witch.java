55decc36d49bf4e990d7a7f6e8d5d240736f1858
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-11-28 22:45:54 -0500

    Update Witch.java

--- v2/Witch.java
+++ v2/Witch.java
@@ -9,7 +9,7 @@ _attack = 0.07;
 
 public static void about()
 {
-  System.out.println("Witch: High strength, low attack. Witch lifts.");
+  System.out.println("Witch: High strength, low attack. All that cauldron-stirring has given the Witch strength rivaling Hercules'.");
 }
 
 }
