59371ee0c44101d2b1b90e615a9fe20964236223
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-11-28 21:36:47 -0500

    Update Elf.java

--- v2/Elf.java
+++ v2/Elf.java
@@ -9,7 +9,7 @@ _attack = 0.1;
 
 public static void about()
 {
-  System.out.println("Elf: High strength, low attack.");
+  System.out.println("Elf: High strength, low attack. Elven armor is imbued with strength-enhancing artifacts. Or maybe it's just all the presents they carry.");
 }
 
 
